47ff4cb5ae0cfed4d30700640a20dc2add87ff39
= <=>
2023-11-26 22:08:57 +0100

    modified app.js and index.html in .

--- app.js
+++ app.js
@@ -32,7 +32,6 @@
 //     })
 //         }
 
-
 // var hashh = window.location.hash.substring(1)
 
 // if (window.location.hash != "") {
@@ -42,7 +41,7 @@
 //             window.location.href = data;
 //     }
 // });
-    
+
 // const shortUrl = () => {
 //   var longurl = geturl();
 //     genhash();
@@ -53,10 +52,41 @@
 
 // get DOM elements
 
-const alert = document.getElementById('alert');
-const longUrlText = document.getElementById('longUrlText').value;
-const shortUrlText = document.getElementById('shortUrlText');
+const apiUrl = "https://api-ssl.bitly.com/v4/shorten";
 
-function shortUrl() {
-    console.log('shortUrlText');
+const alertDOM = document.getElementById("alert");
+const spinner = document.getElementById("spinner");
+const longUrlText = document.getElementById("longUrlText");
+const shortUrlText = document.getElementById("shortUrlText");
+
+async function shortUrl() {
+  const longUrl = longUrlText.value;
+  spinner.classList.remove('hidden')
+  try {
+    const res = await fetch(apiUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json", "Authorization":"Bearer evil-shortener" },
+      body: JSON.stringify({
+        long_url: longUrl,
+      }),
+    });
+
+    const data = await res.json();
+
+    const shortUrl = data.link;
+
+    shortUrlText.innerText = `shortened Url: ${shortUrl}`;
+    
+    simplecopy(shortUrl)
+
+    setTimeout(() => alertDOM.innerText = "longUrl shortened and copied to clipboard", 5000)
+    console.log(data);
+    
+  } catch (error) {
+    alertDOM.innerText = "something went wrong";
+    // shortUrlText.innerHTML =`<button onclick="window.location.reload()" class="bg-teal-950 text-teal-50 p-2 rounded-md hover:bg-teal-700 transition-all duration-300">retry</button>`
+  }
+  
+  spinner.classList.add('hidden')
 }
+
